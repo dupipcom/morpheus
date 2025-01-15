b40b48c000cd394ea918c9430bf12e4a148c420f
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-01-15 18:12:12 +0100

    ar(whim): expand oneiros

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', 'node_modules/@dreampipcom/oneiros/dist/**/*.{js,ts,jsx,tsx,mdx}'],
-  safelist: ['bg-inverse-light', 'bg-inverse-dark', 'min-h-screen'],
+  safelist: ['bg-inverse-light', 'bg-inverse-dark', 'min-h-screen', 'md:min-h-0'],
   darkMode: ['variant', '.dark &:not(.dark .light *, .light .light *)', ''],
   presets: [require('@dreampipcom/oneiros/dist/tailwind.config.js')],
   theme: {
