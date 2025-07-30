f65828aabda310ed792370710bcdbb0f7dea7db0
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-07-30 00:00:59 -0300

    ar(mvp) wip

--- src/components/viewMenu.tsx
+++ src/components/viewMenu.tsx
@@ -9,11 +9,26 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return ['W', weekNo];
+}
 
 export const ViewMenu = ({ active }) =>{
 
   return <NavigationMenu className="flex flex-col center text-center w-full m-auto">
-  <NavigationMenuList>
+  <NavigationMenuList className="flex flex-wrap flex-col md:!flex-row">
       <NavigationMenuItem >
         <NavigationMenuLink active={active === 'dashboard'}>
           <a href="/app/dashboard">Dashboard</a>
@@ -39,11 +54,36 @@ export const ViewMenu = ({ active }) =>{
           <a href="/app/settings">Settings</a>
         </NavigationMenuLink>
       </NavigationMenuItem>
-   
   </NavigationMenuList>
   <div className="my-8">
     <label>Available Balance:</label>
     <Input />
+  </div>
+  <div className="">
+    <div className="flex flex-wrap justify-center">
+      <div className="m-8 flex flex-col">
+        <label>W29</label>
+        <Button>Close week</Button>
+      </div>
+      <div className="m-8 flex flex-col">
+        <label>{getWeekNumber(new Date())}</label>
+        <Button>Close week</Button>
+      </div>
+    </div>
+    <div className="flex flex-wrap justify-center">
+      <div className="m-8 flex flex-col">
+        <label>Friday, Jul 25, 2025</label>
+        <Button>Close day</Button>
+      </div>
+      <div className="m-8 flex flex-col">
+        <label>Friday, Jul 25, 2025</label>
+        <Button>Close day</Button>
+      </div>
+      <div className="m-8 flex flex-col">
+        <label>{new Date().toLocaleString("en-US", {weekday: "long", year: "numeric", month: "short", day: "numeric" })}</label>
+        <Button>Close day</Button>
+      </div>
+    </div>
   </div>
 </NavigationMenu>
 
