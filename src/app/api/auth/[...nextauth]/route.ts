651b59d2046f8c5b5b55b0ae45c44463aac85b2b
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-07-31 12:10:09 -0300

    ar(mvp): ai insights

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email";
 import prisma from "@/lib/prisma";
 
 export const authOptions = {
@@ -11,6 +12,10 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    EmailProvider({
+    server: process.env.EMAIL_SERVER,
+    from: process.env.EMAIL_FROM
+  }),
     // ...add more providers here
   ],
   callbacks: {
