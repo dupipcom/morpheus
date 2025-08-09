5d01a17e2ae76946a1f92d43445a310333a00572
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-08-09 13:11:03 -0300

    ar(fixes)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,8 +77,8 @@ export default function RootLayout({
           {children}
         </GlobalContext.Provider>
         </ClerkProvider>
-        <footer>
-            <div className={`bg-muted grid grid-cols-1 sm:grid-cols-4 md:grid-cols-4 text-[12px] flex w-full flex-col items-start p-8 pb-32`}>
+        <footer className="bg-[#6565cc] text-[#f1cfff]">
+            <div className={`m-auto max-w-[1200px] grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 text-[12px] flex w-full flex-col items-start p-8 pb-32`}>
               <small className="mb-4 ">
                 © 2012—Present Purizu and Remotelys dba DreamPip
                 <br />IVA IT02925300903 
@@ -86,19 +86,19 @@ export default function RootLayout({
                 <br />CNPJ 37.553.462/0001-46
                 <br /><br />
               </small>
-              <div className="dark:text-muted mb-2 flex overflow-hidden max-h-[32px] bg-primary w-[128px]">
-                <img src="/images/europe.png" className="w-[32px] object-cover" />
-                <small className="p-2 text-[8px] font-bold">GDPR Compliant</small>
+              <div>
+                <div className="rounded text-foreground dark:text-muted mb-2 flex overflow-hidden max-h-[32px] bg-primary w-[128px]">
+                  <img src="/images/europe.png" className="w-[32px] object-cover" />
+                  <small className="p-2 text-[8px] font-bold">GDPR Compliant</small>
+                </div>
+                <div className="rounded text-foreground dark:text-muted mb-8 flex overflow-hidden max-h-[32px] bg-primary w-[128px]">
+                  <img src="/images/europe.png" className="w-[32px] object-cover" />
+                  <small className="p-2 text-[8px] font-bold">DORA Ready</small>
+                </div>
               </div>
-
-              <div className="dark:text-muted mb-8 flex overflow-hidden max-h-[32px] bg-primary w-[128px]">
-                <img src="/images/europe.png" className="w-[32px] object-cover" />
-                <small className="p-2 text-[8px] font-bold">DORA Ready</small>
-              </div>
-
-              <small className="">Insights are AI generated via RAG and prompt engineering.
-              <br /><br />We use cookies, and by using this app you agree to it.<br /><br /></small>
               <div className="flex flex-col">
+                              <small className="">Insights are AI generated via RAG and prompt engineering.
+              <br /><br />We use cookies, and by using this app you agree to it.<br /><br /></small>
               <a href="/code" className=""><small>Code</small></a>              
               <a href="/terms" className=""><small>Terms of Service</small></a>
               <a href="/privacy" className=""><small>Privacy Policy</small></a>
