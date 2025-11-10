67c3b9985809d1d1c3cbe2d644785dbe52e1d7e0
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-11-10 08:31:46 -0300

    fix(ar): nits

--- src/views/investView.tsx
+++ src/views/investView.tsx
@@ -9,7 +9,9 @@ export const InvestView = () => {
   return (
     <main className="">
       <div className="container mx-auto px-4 py-6 max-w-4xl sticky top-[115px] z-50">
-        <Construction className="h-16 w-16" />
+        <div className="flex items-center justify-center">
+          <Construction className="h-32 w-32 text-muted-foreground" />
+        </div>
       </div>
     </main>
   )
