aa897b7b901a65230abfe01660f51253ad69df08
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-11-24 20:41:14 -0300

    fix(ar/opt): remove ununsed files

--- src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all utility functions from a single location
-export * from './userUtils'
-export * from './utils'
-export * from './contexts'
-export * from './prisma'
-export * from './openai'
-export * from './payload'
-export * from './cookieManager' 
